f4aeddb2944b2fb49715e4accff3377e5140a44c
ChenTao <bz20100405@163.com>
2019-03-10 17:49:27 +0800

    v2.0

--- test/run.py
+++ test/run.py
@@ -11,14 +11,42 @@ raw = open("script.xml", "r")
 file_context = raw.read()
 root = ET.fromstring(file_context)
 output = ""
-statistic = ""
+statistic = "Benchmark  #Event  #Gate   #Module #prime imp  Time(s) Memory(K)\n"
 
 for roots,dirs,files in os.walk("../raw"):
     break
 
-for file_name in files:
-    if file_name == "das9701.dag" or file_name == "das9601.dag":
-        continue
+file_list = ["baobab3.dag",
+             "chinese.dag",
+             "das9201.dag",
+             "das9202.dag",
+             "das9203.dag",
+             "das9204.dag",
+             "das9205.dag",
+             "das9206.dag",
+             "das9207.dag",
+             "das9208.dag",
+             "edf9201.dag",
+             "edf9202.dag",
+             "edf9205.dag",
+             "edfpa14p.dag",
+             "edfpa14r.dag",
+             "edfpa15b.dag",
+             "edfpa15p.dag",
+             "edfpa15r.dag",
+             "elf9601.dag",
+             "ftr10.dag",
+             "isp9602.dag",
+             "isp9603.dag",
+             "isp9604.dag",
+             "isp9606.dag",
+             "isp9607.dag",
+             "jbd9601.dag"
+            ]
+
+# for file_name in files:
+for file_name in file_list:
+    # file_name = "chinese.dag"
     file_name = file_name[0:len(file_name)-4]
     print(file_name)
     root.getchildren()[0].getchildren()[0].set("input", "../result/"+file_name+".xml")
@@ -34,7 +62,7 @@ for file_name in files:
 
     DEVNULL = open(os.devnull, 'wb', 0)
     p = subprocess.Popen(['/usr/bin/time', '-v'] + 
-          ['./xftar', 'script.xml'], stdout=DEVNULL, stderr=subprocess.PIPE)
+            ['./xftar', 'script.xml'], stdout=DEVNULL, stderr=subprocess.PIPE)
     with p.stderr:
         q = collections.deque(iter(p.stderr.readline, b''))
     rc = p.wait()
@@ -42,6 +70,7 @@ for file_name in files:
 
     r = os.popen("/usr/bin/time -v ./xftar script.xml")
     cmd_output = r.read()
+    print(cmd_output)
     pattern = r"#Gates.*[0-9]*"
     gate_num = re.findall(pattern, cmd_output)[-1]
     pattern = r"#BE.*[0-9]*"
@@ -55,14 +84,13 @@ for file_name in files:
     pattern = r'Maximum resident set size.*[0-9]*'
     resident_size = re.findall(pattern, time_output) [0]
     line_count = os.popen("wc xftar_result/"+file_name+" -l")
+    # basic_event_num.split(" ")[-1] + " " + \
+    # gate_num.split(" ")[-1] + " " + \
     statistic += file_name+ " " +\
-                 line_count.read().split(" ")[0] + " " + \
-                 gate_num.split(" ")[-1] + " " + \
-                 basic_event_num.split(" ")[-1] + " " + \
-                 module_num.split(" ")[-1] + " " + \
-                 user_time.split(" ")[-1] + " " + \
-                 system_time.split(" ")[-1] + " " + \
-                 resident_size.split(" ")[-1] + "\n"
+                    module_num.split(" ")[-1] + " " + \
+                    line_count.read().split(" ")[0] + " " + \
+                    str(float(user_time.split(" ")[-1])+float(system_time.split(" ")[-1])) + " " + \
+                    resident_size.split(" ")[-1] + "\n"
 
 statistic_file = open("statistic", "w")
 statistic_file.write(statistic)
